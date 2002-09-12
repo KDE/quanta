9bbfc45795b14106af37a192a553bddfa700a07b
Andras Mantia <amantia@kde.org>
2002-09-12 10:52:38 +0000

    donationdialog ui files added.

---
+++ dialogs/donationdialog.ui.h
@@ -0,0 +1,14 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename slots use Qt Designer which will
+** update this file, preserving your code. Create an init() slot in place of
+** a constructor, and a destroy() slot in place of a destructor.
+*****************************************************************************/
+
+
+void DonationDialog::init()
+{
+ connect(closeButton,SIGNAL(clicked()), SLOT(accept()));
+}
+
