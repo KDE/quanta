3165daf251735c620f7f48d4ab3aa474e8a78cad
Andras Mantia <amantia@kde.org>
2003-05-05 16:03:35 +0000

    Double inclusion protection. Patch by Frederik Nosi.

--- plugins/framewizard/fwglobal.h
+++ plugins/framewizard/fwglobal.h
@@ -1,3 +1,5 @@
+#ifndef FWGLOBAL_H
+#define FWGLOBAL_H
 
 #include<qstring.h>
 const QString blankpagePosition = "blankpage.html";
@@ -6,4 +8,4 @@ int proxInt(double d);
 
 int cancelledPixels(int n);
 
-
+#endif
