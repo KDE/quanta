f2308024fd4e6a457aabb1df569c85bacd9b3134
Andras Mantia <amantia@kde.org>
2003-07-28 21:03:33 +0000

    Oops, old member variablename used...

--- quantadoc.cpp
+++ quantadoc.cpp
@@ -433,7 +433,7 @@ Document* QuantaDoc::newWrite()
 {
   DTDStruct *dtd = dtds->find(quantaApp->project()->defaultDTD());
   if (!dtd)
-      dtd = dtds->find(qConfig.newFileType);   //fallback, but not really needed
+      dtd = dtds->find(qConfig.defaultDocType);   //fallback, but not really needed
   int i = 1;
   while ( isOpened("file:/"+i18n("Untitled%1.").arg(i)+dtd->defaultExtension)) i++;
 
