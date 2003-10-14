9a93c49a2b5f427b3b8c115b258d61302bb05e94
Andras Mantia <amantia@kde.org>
2003-10-14 11:05:22 +0000

    Fix parsing of unquoted attribute values.

--- parser/tag.cpp
+++ parser/tag.cpp
@@ -167,7 +167,7 @@ void Tag::parse(const QString &p_tagStr, Document *p_write)
         valueStartPos = pos;
         while (pos < strLength && !m_tagStr[pos].isSpace())
           pos++;
-        attr.value = m_tagStr.mid(valueStartPos, pos - valueStartPos -1);
+        attr.value = m_tagStr.mid(valueStartPos, pos - valueStartPos);
       }
       attr.valueLine = m_tagStr.left(valueStartPos).contains('\n') + beginLine;
       if (attr.valueLine == beginLine)
