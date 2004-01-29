00d7d3383116cd590a60ffa7ea7643d9473fa5fc
Andras Mantia <amantia@kde.org>
2004-01-29 18:29:19 +0000

    Hm, this wait is needed here.

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -8,9 +8,11 @@ else
   dcopCommand="dcop quanta-$pid"
 fi
 encoding=`$dcopCommand SettingsIf encoding`
-kmdr-executor $2 &
+a=`kmdr-executor $2 &`
 sleep 2
 pid2=$!
 dcop kmdr-executor-$pid2 KommanderIf setChecked CBMetaChar true
 dcop kmdr-executor-$pid2 KommanderIf setCurrentListItem ComboMetachar "$encoding"
 
+wait $pid2
+echo $a
