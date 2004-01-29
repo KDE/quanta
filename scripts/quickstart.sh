0885db3f372e24208c7b948f3a8b57d63325e7ab
Andras Mantia <amantia@kde.org>
2004-01-29 18:35:50 +0000

    Fix an inculde statement. More work on that script.

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -8,11 +8,9 @@ else
   dcopCommand="dcop quanta-$pid"
 fi
 encoding=`$dcopCommand SettingsIf encoding`
-a=`kmdr-executor $2 &`
+kmdr-executor $2 &
 sleep 2
 pid2=$!
 dcop kmdr-executor-$pid2 KommanderIf setChecked CBMetaChar true
 dcop kmdr-executor-$pid2 KommanderIf setCurrentListItem ComboMetachar "$encoding"
-
 wait $pid2
-echo $a
