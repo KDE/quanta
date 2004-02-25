a514b88000b871e08224d5f79a0fdd6dd32a7680
Andras Mantia <amantia@kde.org>
2004-02-25 14:55:09 +0000

    Don't crash when setting table/body/header/footer attributes for newly created tables [#74949]
    CCMAIL: 74949-done@bugs.kde.org

--- components/tableeditor/tableeditor.ui.h
+++ components/tableeditor/tableeditor.ui.h
@@ -831,15 +831,19 @@ void TableEditor::createNewTable(Document *write, const DTDStruct *dtd)
   m_write = write;
   m_dtd = dtd;
   m_table = new Tag();
+  m_table->dtd = m_dtd;
   newNum++;
   m_table->parse("<table>", m_write);
   m_thead = new Tag();
+  m_thead->dtd = m_dtd;
   newNum++;
   m_thead->parse("<thead>", m_write);
   m_tfoot = new Tag();
+  m_tfoot->dtd = m_dtd;
   newNum++;
   m_tfoot->parse("<tfoot>", m_write);
   m_tbody = new Tag();
+  m_tbody->dtd = m_dtd;
   newNum++;
   m_tbody->parse("<tbody>", m_write);
   //by default the current page is the data handling page
