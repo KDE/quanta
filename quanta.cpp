73f2dcfa4c58ae9f769570bc12b598839f8aebf4
Andras Mantia <amantia@kde.org>
2004-02-27 21:27:01 +0000

    Fix opening of documentation pages with references [#70345]
    CCMAIL: 70345-done@bugs.kde.org

--- quanta.cpp
+++ quanta.cpp
@@ -1664,8 +1664,7 @@ void QuantaApp::selectArea(int line1, int col1, int line2, int col2)
 
 void QuantaApp::openDoc(const QString& url)
 {
-  KURL u;
-  u.setPath(url);
+  KURL u("file:"+url);
   if (u == m_htmlPartDoc->url())
     return;
 
