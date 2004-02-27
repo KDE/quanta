ae17793ade04d2a5ad635915a998e60c270d0146
Andras Mantia <amantia@kde.org>
2004-02-27 23:09:33 +0000

    Hm, sometimes "file:/" is already there (in case of project docs).

--- quanta.cpp
+++ quanta.cpp
@@ -1664,7 +1664,9 @@ void QuantaApp::selectArea(int line1, int col1, int line2, int col2)
 
 void QuantaApp::openDoc(const QString& url)
 {
-  KURL u("file:"+url);
+  if (url.startsWith("/"))
+    url.prepend("file:");
+  KURL u(url);
   if (u == m_htmlPartDoc->url())
     return;
 
