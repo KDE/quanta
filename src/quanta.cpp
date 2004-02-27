838d54581f609750d00a4434b113037c2e70c507
Andras Mantia <amantia@kde.org>
2004-02-27 23:14:07 +0000

    Forwardport: Hm, sometimes "file:/" is already there (in case of project docs)

--- src/quanta.cpp
+++ src/quanta.cpp
@@ -1653,7 +1653,9 @@ void QuantaApp::selectArea(int line1, int col1, int line2, int col2)
 
 void QuantaApp::openDoc(const QString& url)
 {
-  KURL u("file:"+url);
+  if (url.startsWith("/"))
+    url.prepend("file:");
+  KURL u(url);
   if (u == m_htmlPartDoc->url())
     return;
 
