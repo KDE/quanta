9437b7edcc55bc53bd53b0e9bce33887169008ae
Andras Mantia <amantia@kde.org>
2004-07-11 19:20:42 +0000

    CVS_SILENT Update Copyright string.

--- utility/qpevents.h
+++ utility/qpevents.h
@@ -1,14 +1,19 @@
-//
-// C++ Interface: qpevents
-//
-// Description:
-//
-//
-// Author: Andras Mantia <amantia@kde.org>, (C) 2004
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+                          qpevents.h  -  description
+                             -------------------
+    begin                : Sun Jul 11 2004
+    copyright          : (C) 2004 Andras Mantia <amantia@kde.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef QPEVENTS_H
 #define QPEVENTS_H
 
