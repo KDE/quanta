41dd04254f048e1e2e624635bff81b97fda9bb54
Andras Mantia <amantia@kde.org>
2004-07-18 20:21:54 +0000

    Setting the modified status of the document here causes nothing but trouble, so revert this commit.

--- parts/kafka/undoredo.cpp
+++ parts/kafka/undoredo.cpp
@@ -147,7 +147,6 @@ void undoRedo::addNewModifsSet(NodeModifsSet *modifs, int modifLocation)
 #ifdef LIGHT_DEBUG
         kdDebug(25001)<< "undoRedo::addNewModifsSet() - NodeModifsSet type: " << modifLocation << endl;
 #endif
-        m_doc->setModified(true);
 
         QValueList<NodeModif>::iterator it2;
         NodeModifsSet *NMSet;
