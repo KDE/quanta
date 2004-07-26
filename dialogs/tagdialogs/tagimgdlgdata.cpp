13ebf842fa11eab970a3325deab242b259555872
Andras Mantia <amantia@kde.org>
2004-07-26 21:06:11 +0000

    Fix layout of the img tag dialog.
    CCMAIL: 85077-done@bugs.kde.org

--- dialogs/tagdialogs/tagimgdlgdata.cpp
+++ dialogs/tagdialogs/tagimgdlgdata.cpp
@@ -11,100 +11,84 @@
 #include <kurl.h>
 #include <knumvalidator.h>
 
+#include <qlayout.h>
 #include <qspinbox.h>
+
 #include "pictureview.h"
 #include "tagimgdlg.h"
 
 void  TagImgDlg::initDialog(){
-  this->resize(400,320);
-  this->setMinimumSize(0,0);
+  QGridLayout *grid = new QGridLayout( this );
+  grid->setSpacing( 13 );
+  grid->setMargin( 11 );
   labelImgSource= new QLabel(this,"labelImgSource");
-  labelImgSource->setGeometry(10,10,120,30);
-  labelImgSource->setMinimumSize(0,0);
   labelImgSource->setText(i18n("Image source:"));
+  grid->addMultiCellWidget(labelImgSource, 0, 0, 0, 0);
 
   lineImgSource= new KLineEdit(this,"lineImgSource");
-  lineImgSource->setGeometry(130,10,220,25);
-  lineImgSource->setMinimumSize(0,0);
+  grid->addMultiCellWidget(lineImgSource, 0, 0, 1, 3);
 
   buttonImgSource= new QPushButton(this,"NoName");
-  buttonImgSource->setGeometry(355,10,35,25);
-  buttonImgSource->setMinimumSize(0,0);
   buttonImgSource->setText(i18n("..."));
+  grid->addMultiCellWidget(buttonImgSource, 0, 0, 4, 4);
 
   QLabel_4= new QLabel(this,"NoName");
-  QLabel_4->setGeometry(10,40,120,30);
-  QLabel_4->setMinimumSize(0,0);
   QLabel_4->setText(i18n("Width:"));
+  grid->addMultiCellWidget(QLabel_4, 1, 1, 0, 0);
 
   lineWidth= new KLineEdit(this,"NoName");
-  lineWidth->setGeometry(130,40,60,25);
-  lineWidth->setMinimumSize(0,0);
   lineWidth->setValidator( new KFloatValidator( lineWidth ) );
+  grid->addMultiCellWidget(lineWidth, 1, 1, 1, 1);
 
   QLabel_5= new QLabel(this,"NoName");
-  QLabel_5->setGeometry(200,40,90,30);
-  QLabel_5->setMinimumSize(0,0);
   QLabel_5->setText(i18n("Height:"));
+  grid->addMultiCellWidget(QLabel_5, 1, 1, 2, 2);
 
   lineHeight= new KLineEdit(this,"NoName");
-  lineHeight->setGeometry(290,40,60,25);
-  lineHeight->setMinimumSize(0,0);
   lineHeight->setValidator( new KFloatValidator( lineHeight ) );
+  grid->addMultiCellWidget(lineHeight, 1, 1, 3, 3);
 
   buttonRecalcImgSize= new QPushButton(this,"NoName");
-  buttonRecalcImgSize->setGeometry(355,40,35,25);
-  buttonRecalcImgSize->setMinimumSize(0,0);
   buttonRecalcImgSize->setText(i18n("..."));
+  grid->addMultiCellWidget(buttonRecalcImgSize, 1, 1, 4, 4);
 
   QLabel_6= new QLabel(this,"NoName");
-  QLabel_6->setGeometry(10,70,120,30);
-  QLabel_6->setMinimumSize(0,0);
   QLabel_6->setText(i18n("HSpace:"));
+  grid->addMultiCellWidget(QLabel_6, 2, 2, 0, 0);
 
   lineHSpace= new KLineEdit(this,"NoName");
-  lineHSpace->setGeometry(130,70,60,25);
-  lineHSpace->setMinimumSize(0,0);
+  grid->addMultiCellWidget(lineHSpace, 2, 2, 1, 1);
 
   QLabel_7= new QLabel(this,"NoName");
-  QLabel_7->setGeometry(200,70,90,30);
-  QLabel_7->setMinimumSize(0,0);
   QLabel_7->setText(i18n("VSpace:"));
+  grid->addMultiCellWidget(QLabel_7, 2, 2, 2, 2);
 
   lineVSpace= new KLineEdit(this,"NoName");
-  lineVSpace->setGeometry(290,70,60,25);
-  lineVSpace->setMinimumSize(0,0);
+  grid->addMultiCellWidget(lineVSpace, 2, 2, 3, 3);
 
   QLabel_8= new QLabel(this,"NoName");
-  QLabel_8->setGeometry(10,100,120,30);
-  QLabel_8->setMinimumSize(0,0);
   QLabel_8->setText(i18n("Alternate text:"));
+  grid->addMultiCellWidget(QLabel_8, 3, 3, 0, 0);
 
   lineAltText= new KLineEdit(this,"NoName");
-  lineAltText->setGeometry(130,100,260,25);
-  lineAltText->setMinimumSize(0,0);
+  grid->addMultiCellWidget(lineAltText, 3, 3, 1, 4);
 
   QLabel_9= new QLabel(this,"NoName");
-  QLabel_9->setGeometry(10,130,120,30);
-  QLabel_9->setMinimumSize(0,0);
   QLabel_9->setText(i18n("Border:"));
+  grid->addMultiCellWidget(QLabel_9, 4, 4, 0, 0);
 
   spinBorder= new QSpinBox(this,"NoName");
-  spinBorder->setGeometry(130,130,60,25);
-  spinBorder->setMinimumSize(0,0);
   spinBorder->setRange(0,20);
+  grid->addMultiCellWidget(spinBorder, 4, 4, 1, 1);
 
   QLabel_10= new QLabel(this,"NoName");
-  QLabel_10->setGeometry(220,130,70,30);
-  QLabel_10->setMinimumSize(0,0);
   QLabel_10->setText(i18n("Align:"));
+  grid->addMultiCellWidget(QLabel_10, 4, 4, 2, 2);
 
   comboAlign= new QComboBox(this,"NoName");
-  comboAlign->setGeometry(290,130,100,25);
-  comboAlign->setMinimumSize(0,0);
+  grid->addMultiCellWidget(comboAlign, 4, 4, 3, 4);
 
   widgetImg= new PictureView(this);
-  widgetImg->setGeometry(10,170,380,140);
-  widgetImg->setMinimumSize(0,0);
+  grid->addMultiCellWidget(widgetImg, 5, 10, 0, 4);
 
 }
