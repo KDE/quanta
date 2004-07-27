e87baef9e09e2d40f24f9307ac54d9907b3a51f6
Andras Mantia <amantia@kde.org>
2004-07-27 21:38:23 +0000

    Add copyright & license info.

--- project/uploadprofilespage.ui.h
+++ project/uploadprofilespage.ui.h
@@ -6,6 +6,19 @@
 ** init() function in place of a constructor, and a destroy() function in
 ** place of a destructor.
 *****************************************************************************/
+/***********************************************************************
+    begin                : Wed Jul 28 2004
+    copyright            : (C) 2004 by Andras Mantia <amantia@kde.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "projectupload.h"
 
