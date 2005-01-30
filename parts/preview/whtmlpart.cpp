c470188e7899955524d449ef3459917af3cdbecb
Andras Mantia <amantia@kde.org>
2005-01-30 20:20:43 +0000

    Oops, this is only in my local copy...

--- parts/preview/whtmlpart.cpp
+++ parts/preview/whtmlpart.cpp
@@ -178,7 +178,7 @@ void WHTMLPart::slotViewSource()
   QString tempFileName = QFileInfo(*(tmpFile->file())).filePath();
   tmpFile->setAutoDelete(true);
   tmpFile->textStream()->setCodec(QTextCodec::codecForName("utf8"));
-#if KDE_IS_VERSION(3,1,91)  
+#if KDE_IS_VERSION(30,3,92)  
   *(tmpFile->textStream()) << documentSource();
 #else
   *(tmpFile->textStream()) << document().toString().string();
