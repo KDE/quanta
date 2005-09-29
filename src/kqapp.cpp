ec7663d7fa1c5470be3e7e303b42865d71184e03
Dirk Mueller <mueller@kde.org>
2005-09-29 14:33:08 +0000

    SVN_SILENT: fix typo

--- src/kqapp.cpp
+++ src/kqapp.cpp
@@ -13,7 +13,7 @@
     (at your option) any later version.
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, US
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, US
 */
 
 #include <qtimer.h>
