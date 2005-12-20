1481cc071db0a4e448a5c99afdd8b6b5fe65e0ba
Adriaan de Groot <groot@kde.org>
2005-12-20 21:08:20 +0000

    gcc 2.95 has trouble with pointer disambiguation.
    CCMAIL: kde@freebsd.org

--- treeviews/structtreeview.cpp
+++ treeviews/structtreeview.cpp
@@ -776,7 +776,7 @@ bool StructTreeView::acceptDrag(QDropEvent* e) const
         m_marker = current_item;
             
     if(current_item == m_draggedItem ||
-       !(e->provides("text/x-struct_tree_tag_item") || e->source() == this) || !m_marker)
+       !(e->provides("text/x-struct_tree_tag_item") || e->source() == (QWidget *)this) || !m_marker)
     {
         e->accept(false);
         last_accept = false;
