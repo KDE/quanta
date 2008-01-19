e1519071bbc4234e611d1daef1ed6d6a0c151715
Andras Mantia <amantia@kde.org>
2008-01-19 14:47:01 +0000

    Hopefully this will not extract what doesn't need to be translated (but still does what is needed).
    CCMAIL: aacid@kde.org

--- Messages.sh
+++ Messages.sh
@@ -4,17 +4,22 @@ find . -name "*.cc" -print >> files
 find . -name "*.h" -print >> files
 toolbars=`find . -name "*.toolbar.tgz"`
 for toolbar in $toolbars; do 
-   tar Oxfz $toolbar >> extrafiles
+   basename=`echo "$toolbar" | sed "s/.*\///"| sed "s/\\..*$//"`
+   tar Oxfz $toolbar "$basename.toolbar" >> extrafiles
+   tar Oxfz $toolbar "$basename.actions" >> extrafiles2
 done
-cat data/config/actions.rc >> extrafiles
+cat data/config/actions.rc >> extrafiles2
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp 
 $EXTRACTRC `find . -name "*.rc"` >> rc.cpp 
 $EXTRACTRC `find . -name "*.kmdr"` >> rc.cpp 
 cat data/chars | perl -e 'while(<STDIN>) { chomp ; s/\"/\\\"/ ; print "i18n(\"$_\");\n"; }' >> rc.cpp 
 $EXTRACTRC extrafiles >> rc.cpp 
 cat extrafiles | perl -e 'while(<STDIN>) { if (/\<action .* text="(.*)"/) { print "i18n(\"$1\");\n"; }}' >> rc.cpp
+cat extrafiles2 | perl -e 'while(<STDIN>) { if (/\<action .* text="(.*)"/) { print "i18n(\"$1\");\n"; }}' >> rc.cpp
+$EXTRACTATTR extrafiles2 --attr=action,tooltip >> rc.cpp 
+$EXTRACTRC extrafiles2 --tag-group=none --tag=tooltip >> rc.cpp 
 (cd data && $PREPARETIPS >> ../tips.cpp)
 find . -name "*.tag" -print | xargs cat | perl -e 'while(<STDIN>) { if (/\<tag .* comment="(.*)"/) { print "i18n(\"$1\");\n"; }}' >> rc.cpp
 $XGETTEXT rc.cpp tips.cpp `cat files` -o $podir/quanta.pot
-rm -f extrafiles tips.cpp files dirs
+rm -f extrafiles extrafiles2 tips.cpp files dirs
 
