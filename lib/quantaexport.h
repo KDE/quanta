091aacb4ea6f8d02daee329a23e13e4c7ded09d1
Laurent Montel <montel@kde.org>
2008-03-11 21:43:45 +0000

    Update it

--- lib/quantaexport.h
+++ lib/quantaexport.h
@@ -1,50 +1,40 @@
-/*
-   This file is part of the KDevelop platform
-   Copyright (c) 2006 Matt Rogers <mattr@kde.org>
-   Copyright (C) 2004 Jaroslaw Staniek <js@iidea.pl>
+/*  This file is part of the KDE project
+    Copyright (C) 2007 David Faure <faure@kde.org>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
 */
 
 #ifndef QUANTAEXPORT_H
 #define QUANTAEXPORT_H
 
-/* needed for KDE_EXPORT macros */
+/* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 
-/* needed, because e.g. Q_OS_UNIX is so frequently used */
-#ifdef __cplusplus
-# include <QtCore/qglobal.h>
-#endif
-
-#if defined _WIN32 || defined _WIN64
 #ifndef KDEVQUANTA_EXPORT
-# ifdef MAKE_KDEVQUANTA_LIB
+# if defined(MAKE_KDEVQUANTA_LIB)
+   /* We are building this library */ 
 #  define KDEVQUANTA_EXPORT KDE_EXPORT
 # else
+   /* We are using this library */ 
 #  define KDEVQUANTA_EXPORT KDE_IMPORT
 # endif
 #endif
 
+# ifndef KDEVQUANTA_EXPORT_DEPRECATED
+#  define KDEVQUANTA_EXPORT_DEPRECATED KDE_DEPRECATED KDEVQUANTA_EXPORT
+# endif
 
-#else //UNIX
-
-
-/* export statements for unix */
-#define KDEVQUANTA_EXPORT KDE_EXPORT
 #endif
-
-#endif /* QUANTAEXPORT_H*/
-
