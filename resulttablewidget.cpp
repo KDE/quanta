ce0f9d89999d0b7f682cecfb2253be2c1ce87a73
Niko Sams <niko.sams@gmail.com>
2010-03-25 19:53:18 +0000

    Start of an sql plugin.
    The only implemented feature for now is executing the sql query
    in the current document on a (currently hardcoded) QSqlDatabase connection
    and showing the results in a tool view.

---
+++ resulttablewidget.cpp
@@ -0,0 +1,43 @@
+/*
+   Copyright (C) 2010 Niko Sams <niko.sams@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "resulttablewidget.h"
+
+#include <QTableView>
+#include <QHBoxLayout>
+
+namespace Sql {
+
+ResultTableWidget::ResultTableWidget(QWidget* parent)
+    : QWidget(parent)
+{
+    setLayout(new QHBoxLayout());
+    m_view = new QTableView();
+    layout()->addWidget(m_view);
+}
+
+QTableView* ResultTableWidget::view() const
+{
+    return m_view;
+}
+
+
+}
+
+#include "resulttablewidget.moc"
