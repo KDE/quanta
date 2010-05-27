1f6978b05d8ec2117b931669797bd6678ae9926d
Milian Wolff <mail@milianw.de>
2010-05-27 16:19:39 +0200

    remove quanta.bin which is not needed anymore due to the kdevplatform changes

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,8 @@
 
 #include "version.h"
 
+#include "kdevquantaextension.h"
+
 #include <kaboutdata.h>
 #include <kapplication.h>
 #include <kcmdlineargs.h>
@@ -46,16 +48,90 @@
 #include <iostream>
 #include <QtCore/QTextStream>
 
+#include <QSessionManager>
+#include <shell/core.h>
+#include <shell/sessioncontroller.h>
+#include <interfaces/isession.h>
+#include <interfaces/iprojectcontroller.h>
+#include <interfaces/idocumentcontroller.h>
+
+#include <shell/runcontroller.h>
+#include <shell/launchconfiguration.h>
+#include <interfaces/ilauncher.h>
+#include <interfaces/iproject.h>
+
+class QuantaApplication: public KApplication
+{
+
+public:
+  explicit QuantaApplication( bool GUIenabled = true )
+      : KApplication( GUIenabled ) {
+  }
+
+  void saveState( QSessionManager& sm ) {
+    if ( KDevelop::Core::self() && KDevelop::Core::self()->sessionController() ) {
+      sm.setRestartCommand(
+        QStringList() << "quanta" << "-s" << KDevelop::Core::self()->sessionController()->activeSession()->id().toString()
+      );
+    }
+
+    KApplication::saveState( sm );
+  }
+
+};
 
 int main( int argc, char *argv[] )
 {
-  #include "shared_app_init.cpp"
+  static const char description[] =
+    I18N_NOOP("Quanta Plus Web Development Environment");
 
-  //We copy the original argv here, as it seems that KCmdLineArgs changes the arguments ("--style" becomes "-style")
-  QList<QByteArray> argvOrig;
+  static const char othertext[] =
+    I18N_NOOP("Our goal is to be nothing less than the best possible tool for\n \
+    working with tagging and scripting languages.\
+    \n\nQuanta Plus is not in any way affiliated with any commercial\n \
+    versions of Quanta. \
+    \n\nWe hope you enjoy Quanta Plus.\n\n");
 
-  for ( int a = 0; a < argc; ++a )
-    argvOrig << argv[a];
+  KAboutData aboutData("quanta", 0, ki18n("Quanta"),
+                        QUANTA_VERSION, ki18n(description), KAboutData::License_GPL,
+                        ki18n("(c) 1999-2006, The Quanta+ developers"), ki18n(othertext), "http://www.kdewebdev.org");
+
+  aboutData.addAuthor(ki18n("Eric Laffoon"),ki18n("Project Lead - public liaison"), "sequitur@kde.org");
+  aboutData.addAuthor(ki18n("Andras Mantia"),ki18n("Program Lead - bug squisher"), "amantia@kde.org");
+  aboutData.addCredit(ki18n("Jens Herden"),
+                      ki18n("Invaluable member - Help with code cleanup, porting to KDevelop and KDE4, etc."),
+                      "jens@kdewebdev.org");
+
+  //we can't use KCmdLineArgs as it doesn't allow arguments for the debugee
+  //so lookup the --debug switch and eat everything behind by decrementing argc
+  //debugArgs is filled with args after --debug <debuger>
+  QStringList debugArgs;
+  {
+      bool debugFound = false;
+      int c = argc;
+      for (int i=0; i < c; ++i) {
+          if (debugFound) {
+              debugArgs << argv[i];
+          } else if (QString(argv[i]) == "--debug") {
+              if (argc <= i+1) {
+                  argc = i + 1;
+              } else {
+                  i++;
+                  argc = i + 1;
+              }
+              debugFound = true;
+          } else if (QString(argv[i]).startsWith("--debug=")) {
+              argc = i + 1;
+              debugFound = true;
+          }
+      }
+  }
+
+  KCmdLineArgs::init( argc, argv, &aboutData );
+  KCmdLineOptions options;
+  options.add("project <project>", ki18n( "Url to project to load" ));
+  options.add("+files", ki18n( "Files to load" ));
+  options.add("debug <debugger>", ki18n( "Start debugger, for example gdb. The binary that should be debugged must follow - including arguments." ));
 
   KCmdLineArgs::init( argc, argv, &aboutData );
 
@@ -69,7 +145,7 @@ int main( int argc, char *argv[] )
 
   KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
 
-  KApplication app;
+  QuantaApplication app;
 
   if ( args->isSet( "sessions" ) ) {
     QTextStream qout( stdout );
@@ -154,61 +230,163 @@ int main( int argc, char *argv[] )
     session = QUuid::createUuid();
   }
 
-#ifdef Q_WS_WIN
-  QFileInfo fi( QFileInfo( QApplication::applicationFilePath() ).path() + "/quanta.bin.exe" );
+  KDevQuantaExtension::init();
 
-#else
-  QFileInfo fi( QFileInfo( QApplication::applicationFilePath() ).path() + "/quanta.bin" );
+  KSplashScreen* splash = 0;
 
-#endif
-  if ( !fi.exists() ) {
-    QTextStream qerr( stderr );
-    qerr << endl << i18n( "Cannot start Quanta, the Quanta.bin executable is missing in %1. Please fix your Quanta installation.", fi.absolutePath() ) << endl;
-    return -1;
+  QString splashFile = KStandardDirs::locate( "appdata", "pics/quanta-splash.png" );
+
+  if ( !splashFile.isEmpty() ) {
+    QPixmap pm;
+    pm.load( splashFile );
+    splash = new KSplashScreen( pm );
+    splash->show();
   }
 
-  //Forward all arguments, except -s as the internal app doesn't setup -s or --sessions arguments
-  QList<QByteArray> binArgs;
+  if ( !KDevelop::Core::initialize( splash, KDevelop::Core::Default, session ) ) {
+    return 5;
+  }
 
-  for ( int a = 1; a < argvOrig.count(); ++a ) {
-    // note: KCmdLineArgs does not care whether an option gets passed as with one or two dashes
-    if ( argvOrig[a] == "-s" || argvOrig[a] == "--s" || argvOrig[a] == "--cs" || argvOrig[a] == "-cs" ) {
-      ++a;
+  KGlobal::locale()->insertCatalog( KDevelop::Core::self()->componentData().catalogName() );
 
-    } else if ( argvOrig[a] != "--sessions" && argvOrig[a] != "-sessions" ) {
-      binArgs << argvOrig[a];
+  KDevelop::Core* core = KDevelop::Core::self();
+
+  QStringList projectNames = args->getOptionList( "project" );
+
+  if ( !projectNames.isEmpty() ) {
+    foreach( const QString& p, projectNames ) {
+      QFileInfo info( p );
+
+      if ( info.suffix() == "kdev4" ) {
+        // make sure the project is not already opened by the session controller
+        bool shouldOpen = true;
+        KUrl url( info.absoluteFilePath() );
+        foreach( KDevelop::IProject* p, core->projectController()->projects() ) {
+          if ( p->projectFileUrl() == url ) {
+            shouldOpen = false;
+            break;
+          }
+        }
+
+        if ( shouldOpen ) {
+          core->projectController()->openProject( url );
+        }
+      }
     }
   }
 
-#ifdef Q_WS_WIN
-  KProcess proc;
+  if ( args->isSet( "debug" ) ) {
+    if ( debugArgs.isEmpty() ) {
+      QTextStream qerr( stderr );
+      qerr << endl << i18n( "Specify the binary you want to debug." ) << endl;
+      return 1;
+    }
 
-  proc << fi.absoluteFilePath();
+    QString binary = debugArgs.first();
 
-  foreach( const QString &arg, binArgs ) {
-    proc << arg;
+    if ( binary.contains( '/' ) ) {
+      binary = binary.right( binary.lastIndexOf( '/' ) );
+    }
+
+    QString launchName = i18n( "Debug" ) + ' ' + binary;
+
+    KDevelop::LaunchConfiguration* launch = 0;
+    kDebug() << launchName;
+    foreach( KDevelop::LaunchConfiguration *l, core->runControllerInternal()->launchConfigurationsInternal() ) {
+      kDebug() << l->name();
+
+      if ( l->name() == launchName ) {
+        launch = l;
+      }
+    }
+
+    KDevelop::LaunchConfigurationType *type = 0;
+    foreach( KDevelop::LaunchConfigurationType *t, core->runController()->launchConfigurationTypes() ) {
+      kDebug() << t->id();
+
+      if ( t->id() == "Native Application" ) {
+        type = t;
+        break;
+      }
+    }
+
+    if ( !type ) {
+      QTextStream qerr( stderr );
+      qerr << endl << i18n( "Cannot find native launch configuration type" ) << endl;
+      return 1;
+    }
+
+    if ( launch && launch->type()->id() != "Native Application" ) {
+      launch = 0;
+    }
+
+    if ( launch && launch->launcherForMode( "debug" ) != args->getOption( "debug" ) ) {
+      launch = 0;
+    }
+
+    if ( !launch ) {
+      kDebug() << launchName << "not found, creating a new one";
+      QPair<QString, QString> launcher;
+      launcher.first = "debug";
+      foreach( KDevelop::ILauncher *l, type->launchers() ) {
+        if ( l->id() == args->getOption( "debug" ) ) {
+          if ( l->supportedModes().contains( "debug" ) ) {
+            launcher.second = l->id();
+          }
+        }
+      }
+
+      if ( launcher.second.isEmpty() ) {
+        QTextStream qerr( stderr );
+        qerr << endl << i18n( "Cannot find launcher %1" ).arg( args->getOption( "debug" ) ) << endl;
+        return 1;
+      }
+
+      KDevelop::ILaunchConfiguration* ilaunch = core->runController()->createLaunchConfiguration( type, launcher, 0, launchName );
+
+      launch = dynamic_cast<KDevelop::LaunchConfiguration*>( ilaunch );
+    }
+
+    type->configureLaunchFromCmdLineArguments( launch->config(), debugArgs );
+
+    launch->config().writeEntry( "Break on Start", true );
+    core->runControllerInternal()->setDefaultLaunch( launch );
+
+    core->runControllerInternal()->execute( "debug", launch );
+
+  } else {
+    int count = args->count();
+
+    for ( int i = 0; i < count; ++i ) {
+      QString file = args->arg( i );
+      //Allow opening specific lines in documents, like mydoc.cpp:10
+      int lineNumberOffset = file.lastIndexOf( ':' );
+      KTextEditor::Cursor line;
+
+      if ( lineNumberOffset != -1 ) {
+        bool isInt;
+        int lineNr = file.mid( lineNumberOffset + 1 ).toInt( &isInt );
+
+        if ( isInt ) {
+          file = file.left( lineNumberOffset );
+          line = KTextEditor::Cursor( lineNr, 0 );
+        }
+      }
+
+      KUrl url( file );
+
+      if ( url.isRelative() ) {
+        url = KUrl( QDir::currentPath(), file );
+      }
+
+      if ( !core->documentController()->openDocument( url, line ) ) {
+        kWarning() << i18n( "Could not open %1" ) << args->arg( i );
+      }
+    }
+
+    args->clear();
   }
 
-  proc.setEnv( "KDEV_SESSION", session );
-
-  return proc.execute();
-#else
-  QByteArray pathData = fi.absoluteFilePath().toLocal8Bit();
-
-  char **cmd = 0;
-  cmd = ( char** ) realloc( cmd, ( binArgs.count() + 2 ) * sizeof( char* ) );
-  cmd[0] = pathData.data();
-
-  for ( int i = 0; i < binArgs.length(); ++i ) {
-    cmd[i+1] = binArgs[i].data();
-  }
-
-  cmd[binArgs.count() + 1] = 0;
-
-  QByteArray sessionData = session.toLocal8Bit();
-
-  setenv( "KDEV_SESSION", sessionData.data(), true );
-  return execv( qPrintable( fi.absoluteFilePath() ), cmd );
-#endif
+  return app.exec();
 }
 // kate: indent-mode cstyle; space-indent on; indent-width 2; replace-tabs on;  replace-tabs on;
