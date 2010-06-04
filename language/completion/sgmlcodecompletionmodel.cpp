35cbf9b0d9946efd1bcd128889b0ae30b8f200c7
Milian Wolff <mail@milianw.de>
2010-06-04 19:50:34 +0000

    don't crash when start column is larger or equal to text length

--- language/completion/sgmlcodecompletionmodel.cpp
+++ language/completion/sgmlcodecompletionmodel.cpp
@@ -292,8 +292,8 @@ KTextEditor::Range SgmlCodeCompletionModel::completionRange(KTextEditor::View* v
     KTextEditor::Cursor start = position;
     KTextEditor::Cursor end = position;
     QString text = view->document()->line(position.line());
-    if ( text.isEmpty() ) {
-      return Range(0, 0, 0, 0);
+    if ( text.isEmpty() || start.column() >= text.length() ) {
+      return Range(start, end);
     }
     while (start.column() >= 0 && !seps.contains(text.at(start.column()))) {
         start.setColumn(start.column()-1);
