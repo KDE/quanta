d006c19f4e421d7345f023177dbf985b0bf5663a
Milian Wolff <mail@milianw.de>
2010-11-02 10:01:26 +0100

    load messages from .kcfg

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$EXTRACTRC `find . -name \*.rc` `find . -name \*.ui` >>rc.cpp
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/quanta.pot
 rm -f rc.cpp
