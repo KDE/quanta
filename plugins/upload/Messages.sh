bf59d592dab5bf2a95f134a897de985c2e089dcc
Pino Toscano <pino@kde.org>
2010-11-24 22:04:15 +0100

    i18n: do not extract messages from sources in 'tests' subdirectories

--- plugins/upload/Messages.sh
+++ plugins/upload/Messages.sh
@@ -3,5 +3,5 @@ files=`find . -name \*.rc -o -name \*.ui -o -name \*.kcfg`
 if [ "x$files" != "x" ]; then
 	$EXTRACTRC $files >> rc.cpp
 fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevupload.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kdevupload.pot
 rm -f rc.cpp
