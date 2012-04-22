8c06cf24cd0f967a7353e3d5c55edf955a27df00
Niko Sams <niko.sams@gmail.com>
2012-04-22 19:36:32 +0200

    delete plugins that where moved into their own repositories
    - kdev-xdebug
    - kdev-executebrowser
    - kdev-upload

--- debuggers/xdebug/tests/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-function foo() {
-    static $i=0;
-    $i++;
-    echo $i."\n";
-}
-foo();
-foo();
--- plugins/upload/Messages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-files=`find . -name \*.rc -o -name \*.ui -o -name \*.kcfg`
-if [ "x$files" != "x" ]; then
-	$EXTRACTRC $files >> rc.cpp
-fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kdevupload.pot
-rm -f rc.cpp
